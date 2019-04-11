08c016c1009196d31122fdef50efb70f0c605910
cortezle <00029117@uca.edu.sv>
2019-04-11 10:15:59 -0600

    renuevo2

--- app/src/main/java/com/dany/coins/MainActivity.kt
+++ app/src/main/java/com/dany/coins/MainActivity.kt
@@ -1,16 +1,8 @@
 package com.dany.coins
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
-import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
