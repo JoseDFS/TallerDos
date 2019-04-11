562a4d0452aa1c196b9cec44646e583811f2abcf
cortezle <00029117@uca.edu.sv>
2019-04-11 10:12:28 -0600

    renuevo

--- app/src/main/java/com/dany/coins/models/Coin.kt
+++
@@ -1,4 +0,0 @@
-package com.dany.coins.models
-
-data class Coin (val name:String,val country:String, val value:Number,val value_us:Number,val year:Number,val review:String, val isAvailable:Boolean,val img:String){
-}
