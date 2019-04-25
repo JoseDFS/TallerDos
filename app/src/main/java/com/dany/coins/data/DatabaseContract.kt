70ffbf73466a4b3f2d15f615f655dfc1d2133f89
cortezle <00029117@uca.edu.sv>
2019-04-25 16:43:09 -0600

    agregando bases de datos

---
+++ app/src/main/java/com/dany/coins/data/DatabaseContract.kt
@@ -0,0 +1,21 @@
+package com.dany.coins.data
+
+import android.provider.BaseColumns
+
+object DatabaseContract {
+
+    object CoinEntry: BaseColumns{
+
+
+        const val TABLE_NAME = "coin"
+        const val COLUMN_NAME = ""
+        const val COLUMN_COUNTRY =""
+        const val COLUMN_VALUE = ""
+        const val COLUMN_VALUEUS = ""
+        const val COLUMN_YEAR = ""
+        const val COLUMN_REVIEW = ""
+        const val COLUMN_ISAVAILABLE = ""
+        const val COLUMN_IMG = ""
+
+    }
+}
